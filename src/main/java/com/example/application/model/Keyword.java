c4f85f743cda67927998da4580d2ad8857cefcc2
Night-Voyager <18722007@bjtu.edu.cn>
2021-04-22 09:34:42 +0800

    Add keywords field for papers

--- src/main/java/com/example/application/model/Keyword.java
+++ src/main/java/com/example/application/model/Keyword.java
@@ -1,16 +1,12 @@
 package com.example.application.model;
 
-import com.example.application.enumeration.PaperReviewStateEnum;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.Set;
 
 @Entity
@@ -18,23 +14,13 @@ import java.util.Set;
 @Setter
 @Getter
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-public class Paper {
+public class Keyword {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private String title;
+    private String keyword;
 
-    private String filename;
-
-    @ManyToMany(cascade = CascadeType.PERSIST) // 给当前设置的实体操作另一个实体的权限。
-    private Set<Keyword> keywords;
-
-    @CreatedDate
-    private Date createdDate;
-
-    @LastModifiedDate
-    private Date lastModifiedDate;
-
-    private PaperReviewStateEnum state;
+    @ManyToMany(mappedBy = "keywords")
+    private Set<Paper> papers;
 }
---
+++ src/main/java/com/example/application/repository/KeywordRepository.java
@@ -0,0 +1,7 @@
+package com.example.application.repository;
+
+import com.example.application.model.Keyword;
+import org.springframework.data.repository.CrudRepository;
+
+public interface KeywordRepository extends CrudRepository<Keyword, Integer> {
+}
