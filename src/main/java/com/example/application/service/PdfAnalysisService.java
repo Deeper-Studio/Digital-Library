6b7a92279a9d6db90a95338195c4fad73e0d9b91
Thomas-Muller-25 <18726077@bjtu.edu.cn>
2021-04-28 11:45:16 +0800

    Add PdfAnalysisService.java and related dependencies

---
+++ src/main/java/com/example/application/service/PdfAnalysisService.java
@@ -0,0 +1,133 @@
+package com.example.application.service;
+
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class PdfAnalysisService {
+    private static String fileSrc = "D:\\PDFLoaded\\Test.pdf";
+    private static String picFormat = "png";
+    private static String picSuffix = "." + picFormat;
+    private File file;
+    private PDDocument pdDocument;
+    private PDFRenderer pdfRenderer;
+
+    private static int pageNumber = 0;
+
+    private static String title; //标题
+    private static String subject;//主题
+    private static String author;//作者
+    private static String keyWord;
+    private static String creator;
+    private static String creationDate;
+    private static String modificationDate = null;
+
+
+    public void Initial () throws IOException {
+        file = new File(fileSrc);
+        pdDocument = PDDocument.load(file);
+        pdfRenderer = new PDFRenderer(pdDocument);
+
+        //Get properties of PDF file
+        PDDocumentInformation pdDocumentInformation = pdDocument.getDocumentInformation();
+        title = pdDocumentInformation.getTitle();
+        subject = pdDocumentInformation.getSubject();
+        author = pdDocumentInformation.getAuthor();
+        keyWord = pdDocumentInformation.getKeywords();
+        creator = pdDocumentInformation.getCreator();
+        creationDate = pdDocumentInformation.getCreationDate().getTime().toString();
+        modificationDate = pdDocumentInformation.getModificationDate().getTime().toString();
+    }
+
+    //Print properties of the file instead of the pdf content
+    public void showProperties (String fileSrc) {
+        if(fileSrc == null){
+            fileSrc = this.fileSrc;
+        }
+        File file = new File(fileSrc);
+        System.out.println("========= Properties of " + file.getName() + " =========");
+        System.out.println("title:---> " + title);
+        System.out.println("subject:---> " + subject);
+        System.out.println("author:---> " + author);
+        System.out.println("keyword:---> " + keyWord);
+        System.out.println("creator:---> " + creator);
+        System.out.println("creation_date:---> " + creationDate);
+        System.out.println("modification_date:---> " + modificationDate);
+        System.out.println("========= End of properties =========");
+    }
+
+
+    /**
+     *
+     * @param textDest
+     * @throws IOException
+     */
+    public void getContent (String textDest) throws IOException {
+        if(textDest == null){
+            textDest = file.getParent() + "\\" + file.getName() + ".txt";
+        }
+        PDFTextStripper pdfTextStripper = new PDFTextStripper();
+        String text = pdfTextStripper.getText(pdDocument);
+        FileWriter fileWriter = new FileWriter(textDest);
+        fileWriter.write(text);
+        fileWriter.close();
+    }
+
+    public void convetPdfToPic () throws IOException {
+        while (pageNumber < pdDocument.getNumberOfPages()) {
+            BufferedImage image = pdfRenderer.renderImage(pageNumber);
+            ImageIO.write(image, picFormat, new File(file.getAbsolutePath() + "WHOLE_" +
+                    ++pageNumber + picSuffix));
+        }
+        pageNumber = 0;
+    }
+
+    public void convetPdfToPic (int pageNumber) throws IOException {
+        BufferedImage image = pdfRenderer.renderImage(pageNumber);
+        ImageIO.write(image, picFormat, new File(file.getAbsolutePath() + "WHOLE_" +
+                ++pageNumber + picSuffix));
+    }
+
+    public void extractPic () throws IOException {
+        while (pageNumber < pdDocument.getNumberOfPages()) {
+            PDPage pdPage = pdDocument.getPage(pageNumber);
+            PDResources pdResources = pdPage.getResources();
+            for (COSName c : pdResources.getXObjectNames()) {
+                PDXObject o = pdResources.getXObject(c);
+                if (o instanceof org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject) {
+                    ImageIO.write(((org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject) o).getImage(), "png",
+                            new File(file.getAbsolutePath() + "_" + pageNumber +
+                                    "_" + new Random().nextDouble() + picSuffix));
+                }
+            }
+            ++pageNumber;
+        }
+        pageNumber = 0;
+    }
+
+    public void extractPic (int pageNumber) throws IOException {
+        PDPage pdPage = pdDocument.getPage(pageNumber - 1);
+        PDResources pdResources = pdPage.getResources();
+        for (COSName c : pdResources.getXObjectNames()) {
+            PDXObject o = pdResources.getXObject(c);
+            if (o instanceof org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject) {
+                ImageIO.write(((org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject) o).getImage(), picFormat,
+                        new File(file.getAbsolutePath() + "_" + pageNumber +
+                                "_" + new Random().nextDouble() + picSuffix));
+            }
+        }
+    }
+}
+
